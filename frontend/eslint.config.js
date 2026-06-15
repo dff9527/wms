357d7f10b13605122943736aa8ddadf46440e101
Jerry Chang <jerryc9012@gmail.com>
2026-06-15 10:02:30 +0800

    rules set up

---
+++ frontend/eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules', 'coverage'] },
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      // 重點:抓 Rules of Hooks 違規(tsc 抓不到、執行期才會崩)
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      // 既有程式碼用了一些 any / 未用變數,先降級為 warn,避免一次噴太多紅字
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  }
+);
